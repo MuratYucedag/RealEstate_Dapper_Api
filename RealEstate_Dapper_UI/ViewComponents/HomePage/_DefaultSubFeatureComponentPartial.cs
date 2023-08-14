78884bdc2b4a9113ffadd69e44220a705236755a
MuratYucedag <yucedagmurat23@gmail.com>
2023-08-14 08:34:08 +0300

    Ders 12 sonu

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultSubFeatureComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultSubFeatureComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
