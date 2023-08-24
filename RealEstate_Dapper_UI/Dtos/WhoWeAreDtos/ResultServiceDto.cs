ab577aace7378ba02e877faf0c2c87063a6b7693
MuratYucedag <yucedagmurat23@gmail.com>
2023-08-24 17:51:47 +0300

    Ders 16 sonu

---
+++ RealEstate_Dapper_UI/Dtos/WhoWeAreDtos/ResultServiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.WhoWeAreDtos
+{
+    public class ResultServiceDto
+    {
+        public int ServiceID { get; set; }
+        public string ServiceName { get; set; }
+        public bool ServiceStatus { get; set; }
+    }
+}
--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.WhoWeAreDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
-    public class _DefaultWhoWeAreComponentPartial:ViewComponent
+    public class _DefaultWhoWeAreComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultWhoWeAreComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44333/api/WhoWeAreDetail");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDto>>(jsonData);
+                ViewBag.title = value.Select(x => x.Title).FirstOrDefault();
+                ViewBag.subTitle = value.Select(x => x.Subtitle).FirstOrDefault();
+                ViewBag.description1 = value.Select(x => x.Description1).FirstOrDefault();
+                ViewBag.description2 = value.Select(x => x.Description2).FirstOrDefault();
+                return View();
+            }
             return View();
         }
     }
