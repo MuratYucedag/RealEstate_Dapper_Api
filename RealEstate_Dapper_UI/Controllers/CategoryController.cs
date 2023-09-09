695b9009f4b1dc6b5711ffb7b7e6950ebc0b82ab
MuratYucedag <yucedagmurat23@gmail.com>
2023-09-09 10:29:12 +0300

    ....

---
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/AdminLayout/_AdminLayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutHeadComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
