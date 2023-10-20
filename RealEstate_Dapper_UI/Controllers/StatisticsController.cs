81c911719199d7fa92fdc1c5fa2dfcbe88b889e0
MuratYucedag <yucedagmurat23@gmail.com>
2023-10-20 15:46:41 +0300

    Ders 35 sonu

---
+++ RealEstate_Dapper_UI/Controllers/StatisticsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public StatisticsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            #region Statistics1
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44333/api/Statistics/ActiveCategoryCount");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            ViewBag.activeCategoryCount = jsonData;
+            #endregion
+
+            #region Statistics2
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("https://localhost:44333/api/Statistics/ActiveEmployeeCount");
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            ViewBag.activeEmployeeCount = jsonData2;
+            #endregion
+
+            #region Statistics3
+            var client3 = _httpClientFactory.CreateClient();
+            var responseMessage3 = await client3.GetAsync("https://localhost:44333/api/Statistics/ApartmentCount");
+            var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+            ViewBag.apartmentCount = jsonData3;
+            #endregion
+
+            #region Statistics4
+            var client4 = _httpClientFactory.CreateClient();
+            var responseMessage4 = await client4.GetAsync("https://localhost:44333/api/Statistics/AverageProductPriceByRent");
+            var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+            ViewBag.averageProductPriceByRent = jsonData4;
+            #endregion
+
+            #region Statistics5
+            var client5 = _httpClientFactory.CreateClient();
+            var responseMessage5 = await client5.GetAsync("https://localhost:44333/api/Statistics/AverageProductPriceBySale");
+            var jsonData5 = await responseMessage5.Content.ReadAsStringAsync();
+            ViewBag.averageProductPriceBySale = jsonData5;
+            #endregion
+
+            #region Statistics6
+            var client6 = _httpClientFactory.CreateClient();
+            var responseMessage6 = await client6.GetAsync("https://localhost:44333/api/Statistics/AvereageRoomCount");
+            var jsonData6 = await responseMessage6.Content.ReadAsStringAsync();
+            ViewBag.avereageRoomCount = jsonData6;
+            #endregion
+
+            #region Statistics7
+            var client7 = _httpClientFactory.CreateClient();
+            var responseMessage7 = await client7.GetAsync("https://localhost:44333/api/Statistics/CategoryCount");
+            var jsonData7 = await responseMessage7.Content.ReadAsStringAsync();
+            ViewBag.categoryCount = jsonData7;
+            #endregion
+
+            #region Statistics8
+            var client8 = _httpClientFactory.CreateClient();
+            var responseMessage8 = await client8.GetAsync("https://localhost:44333/api/Statistics/CategoryNameByMaxProductCount");
+            var jsonData8 = await responseMessage8.Content.ReadAsStringAsync();
+            ViewBag.categoryNameByMaxProductCount = jsonData8;
+            #endregion
+
+            return View();
+        }
+    }
+}
