33ea938e625204e6916b9e88b4a33864f9448a8e
MuratYucedag <yucedagmurat23@gmail.com>
2024-05-02 17:29:05 +0300

    ...

--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDetailDtos;
 using RealEstate_Dapper_UI.Dtos.ProductDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
@@ -29,7 +30,28 @@ namespace RealEstate_Dapper_UI.Controllers
         [HttpGet]
         public async Task<IActionResult> PropertySingle(int id)
         {
+            id = 1;
+            var client = _httpClientFactory.CreateClient();   
+            var responseMessage = await client.GetAsync("https://localhost:44333/api/Products/GetProductByProductId?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResultProductDto>(jsonData);
+
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("https://localhost:44333/api/ProductDetails/GetProductDetailByProductId?id=" + id);
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            var values2 = JsonConvert.DeserializeObject<GetProductDetailByIdDto>(jsonData2);
+
+            ViewBag.title1 = values.title.ToString();
+            ViewBag.price = values.price;
+            ViewBag.city = values.city;
+            ViewBag.district = values.district;
+            ViewBag.address = values.address;
+            ViewBag.type = values.type;
+
+            ViewBag.bathCount = values2.bathCount;
+            //ViewBag.datediff=values.adv
             return View();
+
         }
     }
 }
---
+++ RealEstate_Dapper_UI/Dtos/ProductDetailDtos/GetProductDetailByIdDto.cs
@@ -0,0 +1,16 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.ProductDetailDtos
+{
+    public class GetProductDetailByIdDto
+    {
+        public int productDetailId { get; set; }
+        public int bedRoomCount { get; set; }
+        public int productSize { get; set; }
+        public int bathCount { get; set; }
+        public int garageSize { get; set; }
+        public string buildYear { get; set; }
+        public decimal price { get; set; }
+        public string location { get; set; }
+        public string videoUrl { get; set; }
+        public int productId { get; set; }
+    }
+}
